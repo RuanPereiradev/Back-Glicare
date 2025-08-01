a8da339b5f570fd81b96a6a2847505013bcb3d4b
RuanPereiradev <ruanpereira@alu.ufc.br>
2025-08-01 13:33:45 -0300

    feat: implementa update e delete no módulo de blog

--- Aviator.Api/Program.cs
+++ Aviator.Api/Program.cs
@@ -1,7 +1,11 @@
 using Aviator.Application;
 using Aviator.Infrastruct;
+using Aviator.Infrastruct.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuração dos serviços
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructServices(builder.Configuration);
 
@@ -11,13 +15,21 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
+// Configuração do pipeline de requisições HTTP
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    // Aplica migrações automaticamente apenas em desenvolvimento
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
+        dbContext.Database.Migrate(); // Aplica migrações pendentes
+    }
 }
 
-// app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
